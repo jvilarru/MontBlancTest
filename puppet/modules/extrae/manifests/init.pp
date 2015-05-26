e6f0bfaf101c74e000da03cd1ab8889e8065ee5e
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-05-26 17:26:27 +0200

    change extrae

--- puppet/modules/extrae/manifests/init.pp
+++ puppet/modules/extrae/manifests/init.pp
@@ -6,29 +6,28 @@ class extrae (  $unwind='',
 		$fft='',
 		$ver = '',
 ){
-	$basicOptions=template("extrae/options.erb")
-	notify{"$basicOptions":}
-	$CFLAGS="-O3 -mcpu=$cortex_cpu -mtune=$cortex_cpu -mfpu=$fpu -funwind-tables -g"
-	#	$sourcePath = $ver ? {
-	#		''      => "/usr/src/extrae/3.1.0"
-	#		default => "/usr/src/extrae/3.1.0/$ver"
-	#	}
-	#	file{"/tmp/extrae-3.1.0.tar.bz2":
-	#		ensure => file,
-	#		source => 'puppet:///modules/extrae/extrae-3.1.0.tar.bz2',
-	#		owner  => 'root',
-	#		group  => 'root',
-	#		mode   => '644'
-	#	}->
-	#	exec{"extract extrae":
-	#		command => 'tar xf /tmp/extrae-3.1.0.tar.bz2',
-	#		path    => '/bin:/usr/bin',
-	#		cwd     => $sourcePath,
-	#	}->
-	#	build_source::install{"extrae":
-	#		sourceFolder => $sourcePath,
-	#		environment => ["CFLAGS=\"$CFLAGS\"","CXXFLAGS=\"$CFLAGS\"","FCFLAGS=\"-O3\""],
-	#		options => $options,
-	#	}
+	#$CFLAGS="-O3 -mcpu=$cortex_cpu -mtune=$cortex_cpu -mfpu=$fpu -funwind-tables -g"
+	$CFLAGS="-O3 -funwind-tables -g"
+	$sourcePath = $ver ? {
+		''      => "/usr/src/extrae/3.1.0"
+		default => "/usr/src/extrae/3.1.0/$ver"
+	}
+	file{"/tmp/extrae-3.1.0.tar.bz2":
+		ensure => file,
+		source => 'puppet:///modules/extrae/extrae-3.1.0.tar.bz2',
+		owner  => 'root',
+		group  => 'root',
+		mode   => '644'
+	}->
+	exec{"extract extrae":
+		command => 'tar xf /tmp/extrae-3.1.0.tar.bz2',
+		path    => '/bin:/usr/bin',
+		cwd     => $sourcePath,
+	}->
+	build_source::install{"extrae":
+		sourceFolder => $sourcePath,
+		environment => ["CFLAGS=\"$CFLAGS\"","CXXFLAGS=\"$CFLAGS\"","FCFLAGS=\"-O3\""],
+		options => template("extrae/options.erb"),
+	}
 
 }
