100d7a77f21ecb27057db2b826c5d7faa229fdfe
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-05-26 17:22:39 +0200

    changes

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,15 +1,5 @@
-class test($env){
-	exec { 'foo':
- 		environment => $env,
-		command => '/bin/echo CFLAGS=$CFLAGS CXXFLAGS=$CXXFLAGS FCFLAGS=$FCFLAGS > /tmp/bar'
-	}
-}
 node 'xubuntu-1404' {
 	include build_source
-	$CFLAGS="-O3 -mcpu=$processor0 -mtune=$processor0 -mfloat-abi=hard -mfpu=$architecture"
-	class{"test":
-		env => ["CFLAGS=\"$CFLAGS\"","CXXFLAGS=\"$CFLAGS\"","FCFLAGS=\"-O3\""]
-	}
 	include ompss
 }
 node 'laptop' {
