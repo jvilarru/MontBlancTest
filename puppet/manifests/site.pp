ce6630cb9e6f68959c78ed04eb8038dc6680b3f4
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-05-26 17:21:54 +0200

    extrae millor i afegim stdlib(just in case)

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,18 +1,25 @@
-class test($env){
-	exec { 'foo':
- 		environment => $env,
-		command => '/bin/echo CFLAGS=$CFLAGS CXXFLAGS=$CXXFLAGS FCFLAGS=$FCFLAGS > /tmp/bar'
-	}
+class test::test(	$unwind='',
+                $papi='',
+                $mpi='',
+                $opencl='',
+                $boost='',
+                $fft='',
+                $ver = '',
+
+){
 }
 node 'xubuntu-1404' {
-	include build_source
-	$CFLAGS="-O3 -mcpu=$processor0 -mtune=$processor0 -mfloat-abi=hard -mfpu=$architecture"
-	class{"test":
-		env => ["CFLAGS=\"$CFLAGS\"","CXXFLAGS=\"$CFLAGS\"","FCFLAGS=\"-O3\""]
-	}
-	include ompss
+	#	include build_source
+	#$CFLAGS="-O3 -mcpu=$processor0 -mtune=$processor0 -mfloat-abi=hard -mfpu=$architecture"
+	#class{"test":
+	#		env => ["CFLAGS=\"$CFLAGS\"","CXXFLAGS=\"$CFLAGS\"","FCFLAGS=\"-O3\""]
+	#}
+	#include ompss
 }
 node 'laptop' {
+	include stdlib
 	include build_source
-	include ompss
+	class{"extrae":
+	}
+#	include ompss
 }
---
+++ puppet/modules/extrae/manifests/init.pp
@@ -0,0 +1,34 @@
+class extrae (  $unwind='',
+		$papi='',
+		$mpi='',
+		$opencl='',
+		$boost='',
+		$fft='',
+		$ver = '',
+){
+	$basicOptions=template("extrae/options.erb")
+	notify{"$basicOptions":}
+	$CFLAGS="-O3 -mcpu=$cortex_cpu -mtune=$cortex_cpu -mfpu=$fpu -funwind-tables -g"
+	#	$sourcePath = $ver ? {
+	#		''      => "/usr/src/extrae/3.1.0"
+	#		default => "/usr/src/extrae/3.1.0/$ver"
+	#	}
+	#	file{"/tmp/extrae-3.1.0.tar.bz2":
+	#		ensure => file,
+	#		source => 'puppet:///modules/extrae/extrae-3.1.0.tar.bz2',
+	#		owner  => 'root',
+	#		group  => 'root',
+	#		mode   => '644'
+	#	}->
+	#	exec{"extract extrae":
+	#		command => 'tar xf /tmp/extrae-3.1.0.tar.bz2',
+	#		path    => '/bin:/usr/bin',
+	#		cwd     => $sourcePath,
+	#	}->
+	#	build_source::install{"extrae":
+	#		sourceFolder => $sourcePath,
+	#		environment => ["CFLAGS=\"$CFLAGS\"","CXXFLAGS=\"$CFLAGS\"","FCFLAGS=\"-O3\""],
+	#		options => $options,
+	#	}
+
+}
