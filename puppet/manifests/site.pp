11631b3d17c71c5818b4068c3609371c0906dd85
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-05-27 21:41:54 +0200

    Al fin compila extrae

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,9 +1,9 @@
 node 'xubuntu-1404' {
-	include build_source
+	build_source::install{ "fftw":
+	        url     => "http://www.fftw.org/fftw-3.3.4.tar.gz",
+	}
+	include extrae
 }
 node 'laptop' {
-	#	build_source::install{ "fftw":
-	#        url     => "http://www.fftw.org/fftw-3.3.4.tar.gz",
-	#}
 	include extrae
 }
