7cdad3d1b27d6044ec6de4eb0e396c3e51106174
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-05-27 11:19:16 +0200

    extract modify

--- puppet/modules/build_source/manifests/archive.pp
+++ puppet/modules/build_source/manifests/archive.pp
@@ -25,11 +25,24 @@ define build_source::archive(
 		owner => 'root',
 		group => 'root',
 	}
-	exec { "Download $title":
-		command => "wget $url -O $name.${extension}",
-		cwd => "/tmp",
-		creates => "/tmp/$name.${extension}" 
-	}->
+	if ($url =~ /^puppet/) {
+		file {"/tmp/$name.${extension}":
+			ensure => file,
+			source => $url,
+			owner  => 'root',
+			group  => 'root',
+			before => Exec["Extract $title"]
+		}
+	}
+	else{
+		exec { "Download $title":
+			command => "wget $url -O $name.${extension}",
+			cwd => "/tmp",
+			creates => "/tmp/$name.${extension}",
+			before => Exec["Extract $title"]
+		}
+
+	}
 	exec { "Extract $title":
        		command => "tar xf /tmp/$name.${extension}",
 		cwd => "$archiveDest",
