a7ef1a860432d889ed4111e74b5b8a90d1cf1cd6
Dani Ruiz <daniel.ruiz@bsc.es>
2015-05-29 15:44:47 +0200

    added boost module

---
+++ puppet/modules/boost/manifests/init.pp
@@ -0,0 +1,21 @@
+class boost {
+	$VERSION="1.58.0"
+	$PREFIX="/opt/boost"
+	$URL="http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz"
+	exec { "wget $URL -O /usr/src/boost.tar.gz":
+		creates => "/usr/src/boost.tar.gz",
+		path    => ["/bin","/usr/bin"],
+	}->
+	exec { "tar xf /usr/src/boost.tar.gz -C /usr/src":
+		creates => "/usr/src/boost_1_58_0"
+		path    => ["/bin","/usr/bin"],
+	}->
+	exec { "./bootstrap.sh --prefix=$PREFIX/$VERSION":
+		path    => ["/bin","/usr/bin","/usr/local/bin","/sbin","/usr/sbin","/usr/local/sbin"],
+		cwd     => "/usr/src/boost_1_58_0",
+	}->
+	exec { "./b2 install":
+		path    => ["/bin","/usr/bin","/usr/local/bin","/sbin","/usr/sbin","/usr/local/sbin"],
+		cwd     => "/usr/src/boost_1_58_0",
+	}
+}
