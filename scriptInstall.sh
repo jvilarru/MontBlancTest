9e435afc09de653d46bc83f00603434a1ec84cd3
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-05-29 10:38:43 +0200

    remove options scriptinstall

--- scriptInstall.sh
+++ scriptInstall.sh
@@ -5,9 +5,6 @@ sudo dpkg -i puppetlabs.deb
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y puppet git 
-if [ "$1" == "--devel" ]; then
-	sudo apt-get install -y vim vim-puppet
-fi
 rm puppetlabs.deb
 git clone https://github.com/uriviba/MontBlancTest.git Mont-Blanc
 sudo puppet apply Mont-Blanc/puppet/manifests --modulepath=Mont-Blanc/puppet/modules --test
