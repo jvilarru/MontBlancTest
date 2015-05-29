a9af6e29a61d48ee6f73d6d99e5c0e5ce9b6c6f8
Dani Ruiz <daniel.ruiz@bsc.es>
2015-05-29 12:27:29 +0200

    corrected error with templates path

--- puppet/modules/papi/manifests/init.pp
+++ puppet/modules/papi/manifests/init.pp
@@ -4,7 +4,7 @@ class papi{
 	build_source::install{"papi":
 		url          => "http://icl.cs.utk.edu/projects/papi/downloads/papi-5.4.1.tar.gz",
 		environment  => ["CFLAGS=$CFLAGS","FFLAGS=$FFLAGS"],
-		options      => template("extrae/options.erb"),
+		options      => template("papi/options.erb"),
 		version      => "5.4.1",
 	}
 }
