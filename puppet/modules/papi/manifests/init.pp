6e0bfa4b8da7644687b9652071871eba085c6bfa
Dani Ruiz <daniel.ruiz@bsc.es>
2015-05-29 15:29:34 +0200

    using new variable to indicate configure location

--- puppet/modules/papi/manifests/init.pp
+++ puppet/modules/papi/manifests/init.pp
@@ -5,6 +5,6 @@ class papi{
 		url          => "http://icl.cs.utk.edu/projects/papi/downloads/papi-5.4.1.tar.gz",
 		environment  => ["CFLAGS=$CFLAGS","FFLAGS=$FFLAGS"],
 		version      => "5.4.1",
-		workDir      => "src",
+		prefixConfigure => "src",
 	}
 }
