2658466776199ab8aa5dea77a82a500ca0caf5c4
Dani Ruiz <daniel.ruiz@bsc.es>
2015-05-29 12:35:42 +0200

    papi is not using any option template

--- puppet/modules/papi/manifests/init.pp
+++ puppet/modules/papi/manifests/init.pp
@@ -4,7 +4,6 @@ class papi{
 	build_source::install{"papi":
 		url          => "http://icl.cs.utk.edu/projects/papi/downloads/papi-5.4.1.tar.gz",
 		environment  => ["CFLAGS=$CFLAGS","FFLAGS=$FFLAGS"],
-		options      => template("papi/options.erb"),
 		version      => "5.4.1",
 		workDir      => "src",
 	}
