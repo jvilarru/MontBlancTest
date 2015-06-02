f13bc5bb50c434c07debdf0e92089369a1a586a1
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-06-02 12:12:38 +0200

    gnu complier stetic changes

--- puppet/modules/gnu_compiler/manifests/init.pp
+++ puppet/modules/gnu_compiler/manifests/init.pp
@@ -1,23 +1,29 @@
 #dependences = bison, flex
 class gnu_compiler {
+	#GMP
+	$GMP_ver = "6.0.0a"
+	$GMP_dest = "/opt/gmp/$GMP_ver"
 	$CFLAGS_GMP="-O3"
+	build_source::install{"gmp":
+		url         => "https://gmplib.org/download/gmp/gmp-6.0.0a.tar.xz"
+		environment => ["CFLAGS=$CFLAGS_GMP"],
+		options     => template("$module_name/options_gmp.erb"),
+		dest        => $GMP_dest,
+		dependences => ["bison","flex"]
+	}
+	
+	#MPFR
 	$CFLAGS_MPFR="-O3 -fPIC"
 	$CFLAGS_MPC="-O3 -fPIC"
 	$CFLAGS_ISL="-O3 -fPIC"
 	$CFLAGS_CLOOG="-O3 -fPIC"
 	$CFLAGS_GCC="-O3"
-	build_source::install{"gmp":
-		url          => "https://gmplib.org/download/gmp/gmp-6.0.0a.tar.xz"
-		environment  => ["CFLAGS=$CFLAGS_GMP"],
-		options      => template("$module_name/options_gmp.erb"),
-		version      => "6.0.0a",
-		dependences => ["bison","flex"]
-	}->
 	build_source::install{"mpfr":
-		url          => "http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.xz"
-		environment  => ["CFLAGS=$CFLAGS_MPFR"],
-		options      => template("$module_name/options_mpfr.erb"),
-		version      => "3.1.2",
+		url         => "http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.xz"
+		environment => ["CFLAGS=$CFLAGS_MPFR"],
+		options     => template("$module_name/options_mpfr.erb"),
+		version     => "3.1.2",
+		require     =>  Build_source::Install["gmp"]
 	}->
 	build_source::install{"mpc":
 		url          => "ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
