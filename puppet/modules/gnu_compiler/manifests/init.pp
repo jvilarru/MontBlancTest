b4a01522602c2ecfabc2b3a5f7e4449b22c78964
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-06-02 12:20:34 +0200

    change in gnu compiler

--- puppet/modules/gnu_compiler/manifests/init.pp
+++ puppet/modules/gnu_compiler/manifests/init.pp
@@ -14,39 +14,47 @@ class gnu_compiler {
 	
 	#MPFR
 	$CFLAGS_MPFR="-O3 -fPIC"
-	$CFLAGS_MPC="-O3 -fPIC"
-	$CFLAGS_ISL="-O3 -fPIC"
-	$CFLAGS_CLOOG="-O3 -fPIC"
-	$CFLAGS_GCC="-O3"
 	build_source::install{"mpfr":
 		url         => "http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.xz"
 		environment => ["CFLAGS=$CFLAGS_MPFR"],
 		options     => template("$module_name/options_mpfr.erb"),
 		version     => "3.1.2",
 		require     =>  Build_source::Install["gmp"]
-	}->
+	}
+	#MPC
+	$CFLAGS_MPC="-O3 -fPIC"
 	build_source::install{"mpc":
 		url          => "ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
 		environment  => ["CFLAGS=$CFLAGS_MPC"],
 		options      => template("$module_name/options_mpc.erb"),
 		version      => "1.0.3",
-	}->
+		require     =>  Build_source::Install["mpfr"]
+	}
+	#ISL
+	$CFLAGS_ISL="-O3 -fPIC"
 	build_source::install{"isl":
 		url          => "http://mirror1.babylon.network/gcc/infrastructure/isl-0.14.tar.bz2"
 		environment  => ["CFLAGS=$CFLAGS_ISL"],
 		options      => template("$module_name/options_isl.erb"),
 		version      => "0.14",
-	}->
+		require     =>  Build_source::Install["mpc"]
+	}
+	#CLOOG
+	$CFLAGS_CLOOG="-O3 -fPIC"
 	build_source::install{"cloog":
 		url          => "http://www.bastoul.net/cloog/pages/download/cloog-0.18.3.tar.gz"
 		environment  => ["CFLAGS=$CFLAGS_CLOOG"],
 		options      => template("$module_name/options_cloog.erb"),
 		version      => "0.18.3",
-	}->
+		require     =>  Build_source::Install["isl"]
+	}
+	#GCC
+	$CFLAGS_GCC="-O3"
 	build_source::install{"gcc":
 		url          => "http://mirror1.babylon.network/gcc/releases/gcc-5.1.0/gcc-5.1.0.tar.gz"
 		environment  => ["CFLAGS=$CFLAGS_GCC"],
 		options      => template("$module_name/options_gcc.erb"),
 		version      => "5.1.0",
+		require     =>  Build_source::Install["cloog"]
 	}
 }
