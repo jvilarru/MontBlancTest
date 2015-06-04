14c7e908318c164840cc6ec87c8d4167ae5c3408
Dani Ruiz <daniel.ruiz@bsc.es>
2015-06-04 23:36:05 +0200

    fftw module added

---
+++ puppet/modules/fftw/manifests/init.pp
@@ -0,0 +1,33 @@
+#dependences = gfortran
+class fftw{
+	$CFLAGS="-O3"
+	$FFLAGS="-O3"
+	build_source{"$module_name single precision":
+		url      => "http://www.fftw.org/fftw-3.3.4.tar.gz",
+		srcDest  => "/usr/src/fftw/3.3.4",
+		env	     => ["CFLAGS=$CFLAGS","FFLAGS=$FFLAGS"],
+		buildDir => "single_precision",
+		options  => "--enable-fma --enable-threads --enable-shared --enable-single",
+		dest     => "/opt/fftw/3.3.4_single",
+		version  => "3.3.4_single",
+		packages => ["gfortran"]
+	}
+	build_source{"$module_name double precision":
+		url      => "http://www.fftw.org/fftw-3.3.4.tar.gz",
+		srcDest  => "/usr/src/fftw/3.3.4",
+		env	     => ["CFLAGS=$CFLAGS","FFLAGS=$FFLAGS"],
+		buildDir => "double_precision",
+		dest     => "/opt/fftw/3.3.4",
+		options  => "--enable-fma --enable-threads --enable-shared",
+		version  => "3.3.4",
+	}
+
+	exec { "merge libraries":
+		user    => 'root',
+		group   => 'root',
+		path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+		command => "cp -a /opt/fftw/3.3.4_single/lib/lib* /opt/fftw/3.3.4/lib",
+		require => [Build_source["$module_name single precision"],Build_source["$module_name double precision"]],
+		creates => "/opt/fftw/3.3.4/lib/libfftw3f.la"
+	}
+}
