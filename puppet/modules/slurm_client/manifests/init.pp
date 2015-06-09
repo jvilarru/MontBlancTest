022b8e081b11714d1795f47acd4f085996354c15
Dani Ruiz <daniel.ruiz@bsc.es>
2015-06-09 16:04:09 +0200

    added first version of SLURM client module without any kind of configuration

---
+++ puppet/modules/slurm_client/manifests/init.pp
@@ -0,0 +1,27 @@
+class slurm {
+	# installation
+	build_source{"$module_name":
+		url      => "$module_name/slurm-14.11.7.tar.bz2",
+		options  => template("$module_name/options.erb"),
+		version  => "14.11.7",
+		packages => ["munge","libmunge-dev","libcr-dev","libpam0g-dev","libssl-dev","openssl","libmysqld-dev","mysql-common","pkg-config","libxml2-dev","hwloc","libmysqlclient-dev","libhwloc-dev"]
+	}
+	
+	# SLURM user configuration
+	group { "$module_name group":
+		name   => "slurm",
+		ensure => present,
+		gid    => "999",
+		system => "yes"
+	}
+	user {  "$module_name user":
+		name    => "slurm",
+		ensure  => present,
+		uid     => '999',
+		system  => "yes",
+		gid     => '999',
+		require => Group["$module_name group"]
+	}
+
+	# SLURM needed folders
+}
